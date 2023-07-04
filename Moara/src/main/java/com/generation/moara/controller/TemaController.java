6afcf70f4791f8e084d2dc8f1022d073ee854ea3
rita_@Almeida <rita_aja92@hotmail.com>
2023-07-04 16:18:04 -0300

    @Maysouzac @Kauanhassani @RitaAlmeidah - Task5 - CRUD Postagem

--- Moara/src/main/java/com/generation/moara/controller/TemaController.java
+++ Moara/src/main/java/com/generation/moara/controller/TemaController.java
@@ -54,9 +54,11 @@ public class TemaController {
 	@PutMapping
 	public ResponseEntity<Tema> put(@Valid @RequestBody Tema tema) {
 		return temaRepository.findById(tema.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema)))
+				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED)
+				.body(temaRepository.save(tema)))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
+	
 
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
