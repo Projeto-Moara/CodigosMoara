4fa4b6e9c733d8fdd6aeff8ba916b6e81d7afb77
rita_@Almeida <rita_aja92@hotmail.com>
2023-07-04 11:45:03 -0300

    @Maysouzac @Kauanhassani @RitaAlmeidah - Task5 - CRUD Postagem

---
+++ Moara/src/main/java/com/generation/moara/repository/PostagemRepository.java
@@ -0,0 +1,14 @@
+package com.generation.moara.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.moara.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	
+	public List<Postagem> findAllByTextoContainingIgnoreCase(@Param ("texto")String texto);
+
+}
